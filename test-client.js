2a2b9d461a42a8123033c73da30fa74b002995e2
David Enebeli <davidenebeli@Davids-MacBook-Pro.local>
2025-08-25 13:57:24 +0100

    fix: type error

--- test-client.js
+++ test-client.js
@@ -88,7 +88,7 @@ async function runTests() {
     const sendResult = await testEndpoint('POST', '/send-eth', {
         to,
         // 0.122 ETH
-        valueEth: '1AA535D3D0C0000',
+        valueEth: 'ox1AA535D3D0C0000',
         // example gas price and limit; consider fetching from RPC for accuracy
         gasPriceWei: '0x3b9aca00', // 1 gwei
         gasLimit: '0x5208',        // 21000
